faa8dbeba8777dfd2a2664a42f87d4b693a8e707
Maksim_Ermilov <maks.ermilov.03@mail.ru>
2023-09-05 15:03:55 +0300

    RenameProject

--- Sked-Authoriztion/src/DAL/Infrastructure/IUserDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using MongoDB.Driver;
-using SkedAuthoriztion.DAL.Entities;
-
-namespace SkedAuthoriztion.DAL.Infrastructure;
-
-public interface IUserDbContext
-{
-    IMongoCollection<User> Users { get; }
-}
